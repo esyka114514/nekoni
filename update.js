6d1657d969805eebcc9bb44019cca4fb8c39cd07
Esyka <2833598659@qq.com>
2026-05-03 08:59:17 +0800

    fix: 在版本检查相同时添加进程退出
    当检测到版本相同时，添加 process.exit(0) 确保脚本正确退出

--- update.js
+++ update.js
@@ -144,6 +144,7 @@ const updateTime = getUpdateTime();
 if (oldCommitId === newCommitId) {
     logger.info(`Nekoni已是最新版本\n最后更新时间: ${updateTime}`);
     fs.unlinkSync(packageJsonBakPath);
+    process.exit(0);
 }
 
 logger.info(`Nekoni已更新\n最后更新时间: ${updateTime}`);
